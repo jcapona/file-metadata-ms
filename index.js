ba2a0133c500a0dce60963fe8730ca867d9a4050
Jorge Capona <jcapona@gmail.com>
2016-01-14 14:11:52 -0300

    Uses multer package to upload file

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var util = require('util');
-var formidable = require('formidable');
+var multer  = require('multer');
+var upload = multer({ dest: './uploads' });
 
 // ROUTES 
 app.set('port', (process.env.PORT || 5000));
-//app.set('view engine', 'jade');
 
 app.get("/", function(request, response) {
     response.sendFile(path.join(__dirname+'/views/index.html'));
@@ -16,13 +16,10 @@ app.get("*", function(request, response) {
   response.redirect("/");
 });
 
-app.post("/api/analyze/", function(req, res){
-    var form = new formidable.IncomingForm();
-    form.parse(req, function(err, fields, files) {
-        res.json(files.file);
-    });
-});
-
 app.listen(app.get('port'));
+
+app.post('/upload/', upload.single('upfile'), function (req, res) {
+  res.json({"name": req.file.originalname,"type": req.file.mimetype,"size": req.file.size});
+});
 
 
