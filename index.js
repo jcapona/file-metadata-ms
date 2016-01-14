7ae743133def17daf027302c124ba2b02749452d
Jorge Capona <jcapona@gmail.com>
2016-01-14 11:13:37 -0300

    Displays file json, using formidable

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var util = require('util');
+var formidable = require('formidable');
 
 // ROUTES 
 app.set('port', (process.env.PORT || 5000));
@@ -14,4 +16,13 @@ app.get("*", function(request, response) {
   response.redirect("/");
 });
 
+app.post("/api/analyze/", function(req, res){
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files) {
+        res.json(files.file);
+    });
+});
+
 app.listen(app.get('port'));
+
+
